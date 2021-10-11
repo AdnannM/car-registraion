cdd30f4cb23bd4cfc1aa861d06a8ecab689ae802
AdnannM <48089787+AdnannM@users.noreply.github.com>
2021-10-11 11:54:33 +0200

    Set up project and add data

---
+++ registracijaVozila/src/main/java/com/api/registracijaVozila/dto/VoziloDto.java
@@ -0,0 +1,25 @@
+package com.api.registracijaVozila.dto;
+
+
+public class VoziloDto {
+
+	private Integer id;
+	private String registracijskaOznaka;
+	
+	
+	public Integer getId() {
+		return id;
+	}
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	public String getRegistracijskaOznaka() {
+		return registracijskaOznaka;
+	}
+	public void setRegistracijskaOznaka(String registracijskaOznaka) {
+		this.registracijskaOznaka = registracijskaOznaka;
+	}
+	
+	
+	
+}
