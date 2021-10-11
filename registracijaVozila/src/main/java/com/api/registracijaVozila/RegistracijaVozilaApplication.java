37210ddd394dca49d0f4e581746423ba07209cdb
AdnannM <48089787+AdnannM@users.noreply.github.com>
2021-10-11 10:22:26 +0200

    registraion-car spring boot

---
+++ registracijaVozila/src/main/java/com/api/registracijaVozila/RegistracijaVozilaApplication.java
@@ -0,0 +1,13 @@
+package com.api.registracijaVozila;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RegistracijaVozilaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RegistracijaVozilaApplication.class, args);
+	}
+
+}
