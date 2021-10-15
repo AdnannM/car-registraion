8b4e88120a83f398f6656d89c11ae58f097ae157
AdnannM <48089787+AdnannM@users.noreply.github.com>
2021-10-15 09:25:57 +0200

    CarSpecs GET POST PUT DELETE

--- registracijaVozila/src/main/java/com/api/service/KarakteristikeVozilaServiceImpl.java
+++ registracijaVozila/src/main/java/com/api/service/KarakteristikeVozilaServiceImpl.java
@@ -2,11 +2,13 @@ package com.api.service;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.api.dao.KarakteristikeVozilaDao;
+
 import com.api.dto.KarakteristikeVozilaDto;
 import com.api.repository.KarakteristikeVozilaRepository;
 
@@ -43,21 +45,62 @@ public class KarakteristikeVozilaServiceImpl implements KarakteristikeVozilaServ
 
 	@Override
 	public KarakteristikeVozilaDto createKarakteristikeVozila(KarakteristikeVozilaDto karakteristikeVozila) {
-		// TODO Auto-generated method stub
-		return null;
+		
+		KarakteristikeVozilaDao noveKarakteristikeVozila = new KarakteristikeVozilaDao();
+		noveKarakteristikeVozila.setId(karakteristikeVozila.getId());
+		noveKarakteristikeVozila.setBoja(karakteristikeVozila.getBoja());
+		noveKarakteristikeVozila.setBrojSasije(karakteristikeVozila.getBrojSasije());
+		noveKarakteristikeVozila.setSnagaMotora(karakteristikeVozila.getSnagaMotora());
+		noveKarakteristikeVozila.setTipVozila(karakteristikeVozila.getTipVozila());
+		
+		KarakteristikeVozilaDao spremiKarakteristikeVozila = karakteristikeVozilaRepository.save(noveKarakteristikeVozila);
+		
+		KarakteristikeVozilaDto karakteristikeVozilaDto = new KarakteristikeVozilaDto();
+		karakteristikeVozilaDto.setId(spremiKarakteristikeVozila.getId());
+		karakteristikeVozilaDto.setBoja(spremiKarakteristikeVozila.getBoja());
+		karakteristikeVozilaDto.setBrojSasije(spremiKarakteristikeVozila.getBrojSasije());
+		karakteristikeVozilaDto.setSnagaMotora(spremiKarakteristikeVozila.getSnagaMotora());
+		karakteristikeVozilaDto.setTipVozila(spremiKarakteristikeVozila.getTipVozila());
+		
+		return karakteristikeVozilaDto;
 	}
 
 	@Override
-	public KarakteristikeVozilaDto updateKarakteristikeVozila(Integer id,
-			KarakteristikeVozilaDto karakteristikeVozila) {
-		// TODO Auto-generated method stub
-		return null;
+	public KarakteristikeVozilaDto updateKarakteristikeVozila(Integer id,KarakteristikeVozilaDto karakteristikeVozila) {
+		
+		Optional<KarakteristikeVozilaDao> noveKarakteristikeIzBazeResult = Optional.of(karakteristikeVozilaRepository.findById(id))
+				.orElseThrow(() -> new IllegalStateException("New Specs with this id " + id + "does not exist"));
+		
+		KarakteristikeVozilaDao noveKarakteristikeIzBaze = noveKarakteristikeIzBazeResult.get();
+		noveKarakteristikeIzBaze.setId(karakteristikeVozila.getId());
+		noveKarakteristikeIzBaze.setBoja(karakteristikeVozila.getBoja());
+		noveKarakteristikeIzBaze.setBrojSasije(karakteristikeVozila.getBrojSasije());
+		noveKarakteristikeIzBaze.setSnagaMotora(karakteristikeVozila.getSnagaMotora());
+		noveKarakteristikeIzBaze.setTipVozila(karakteristikeVozila.getTipVozila());
+		
+		
+		KarakteristikeVozilaDao saveKarakteristikeVozila = karakteristikeVozilaRepository.save(noveKarakteristikeIzBaze);
+		
+		KarakteristikeVozilaDto karakteristikeVozilaDto = new KarakteristikeVozilaDto();
+		karakteristikeVozilaDto.setId(saveKarakteristikeVozila.getId());
+		karakteristikeVozilaDto.setBoja(saveKarakteristikeVozila.getBoja());
+		karakteristikeVozilaDto.setBrojSasije(saveKarakteristikeVozila.getBrojSasije());
+		karakteristikeVozilaDto.setSnagaMotora(saveKarakteristikeVozila.getSnagaMotora());
+		karakteristikeVozilaDto.setTipVozila(saveKarakteristikeVozila.getTipVozila());
+		
+		return karakteristikeVozilaDto;
 	}
 
 	@Override
 	public void deleteKarakteristikeVozila(Integer id) {
-		// TODO Auto-generated method stub
-		
+		if(karakteristikeVozilaRepository.getById(id).getId().equals(id)) {
+			boolean exist = karakteristikeVozilaRepository.existsById(id);
+			
+			if(!exist) {
+				throw new IllegalStateException("Spec with this id " + id + "does not exist");
+			}
+			
+			karakteristikeVozilaRepository.deleteById(id);
+		}
 	}
-
 }
